6bec2262d52ced9e30e53a4012989219a8322217
floradb26 <debruyne.f@outlook.com>
2021-12-19 16:53:55 +0100

    added dicitonary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,13 @@
 import React, { useState } from "react"; 
+import axios from "axios";
 import "./Dictionary.css"
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState(null);   
+
+    function handleResponse(response) {
+      console.log(response.data[0]);  
+    }
 
     function search(event) {
         event.preventDefault();
@@ -13,6 +18,9 @@ export default function Dictionary() {
         setKeyword(event.target.value);
     }
     
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+
     return (
    <div className="Dictionary">
     <form onSubmit={search}>
