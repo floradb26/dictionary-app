72589a292f95a69855eef3d6450a59fc3ede82b3
floradb26 <debruyne.f@outlook.com>
2021-12-19 16:16:50 +0100

    Added event

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Dictionary from "./Dictionary"
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <a href="/" className="btn btn-primary btn-lg" rel="noreferrer">Hello</a>
+        <main>
+        <Dictionary />
+      </main>
       </header>
+      <footer className="footer">Coded by Floriane Ramirez De Bruyne</footer>
+      </div>
     </div>
   );
 }
