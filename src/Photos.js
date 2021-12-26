4c9ddee843ea28b41aa130ae5bced2aba07c5f2f
floradb26 <debruyne.f@outlook.com>
2021-12-26 19:47:01 +0100

    alt information image

--- src/Photos.js
+++ src/Photos.js
@@ -13,7 +13,7 @@ export default function Photos(props) {
             return (
                 <div className="col-4" key={index}>
                   <a href={photo.src.original} target="_blank" rel="noreferrer">
-                    <img src={photo.src.landscape} className="img-fluid" /></a>      
+                    <img src={photo.src.landscape} className="img-fluid" alt="word searched illustration" /></a>      
                  </div>
                  );
                 })}
